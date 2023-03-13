9bf70f658c87c885cfe17d8c6722360973634f03
Cami Fogu <fogucamila@gmail.com>
2023-03-13 17:10:59 -0300

    clima

--- js/login.js
+++ js/login.js
@@ -165,3 +165,19 @@ boton_login.addEventListener("click", function () {
     });
   }
 });
+
+//CLIMA
+let key = "bbf8893c6e8030e157bb633d11a66e17";
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires&units=metric&lang=es&appid=" +
+    key
+)
+  .then((response) => response.json())
+  .then((data) => {
+    let resultado_clima = document.getElementById("resultado_clima");
+    continuar = document.createElement("div");
+    continuar.innerHTML = `<span>El clima en tu ciudad es:</span>
+    <p>Temp: ${data.main.temp} - ${data.weather[0].description}</p>`;
+    resultado_clima.append(continuar);
+  });
